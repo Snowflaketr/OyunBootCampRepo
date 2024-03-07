1c6c1d71165e564cf2dfa662c9bb82c58e3632c5
Snowflaketr <162337984+Snowflaketr@users.noreply.github.com>
2024-03-07 22:49:43 +0300

    tamamlandı

---
+++ GameDevBoot/Assets/Ders1/FirstLessonCode.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstLessonCode : MonoBehaviour
+{
+    // Deðiþkenler
+
+    // Cümle, paragraf, isim, özel ad...= String
+    // sayý,rakam...= Ýnt
+    // Küsüratlý sayý,ondalýklý sayý... = Float,Double
+    // Tek Karakter,alfabe... = char
+    // Ýki seçenekli durumlar / doðru-yanlýþ= bool
+    string benimadým = "Ali BALTAKESEN";
+    string universitem = "Atatürk Üniversitesi";
+
+    char cinsiyetim= 'E';
+
+    int yasým = 31;
+
+    bool hayattamý=true;
+
+    
+
+
+}

ec43d964d46b78cb3c3c0d94736a6731134d862d
Snowflaketr <162337984+Snowflaketr@users.noreply.github.com>
2024-03-10 02:53:50 +0300

    güncel

--- GameDevBoot/Assets/Ders1/FirstLessonCode.cs
+++ GameDevBoot/Assets/Ders1/FirstLessonCode.cs
@@ -11,16 +11,70 @@ public class FirstLessonCode : MonoBehaviour
     // Küsüratlý sayý,ondalýklý sayý... = Float,Double
     // Tek Karakter,alfabe... = char
     // Ýki seçenekli durumlar / doðru-yanlýþ= bool
-    string benimadým = "Ali BALTAKESEN";
+    string adým = "Ali";
+    string soyadým = "BALTAKESEN";
     string universitem = "Atatürk Üniversitesi";
-
-    char cinsiyetim= 'E';
-
+    string adres = "Yunus Emre Mah. Sincan/Ankara";
+    char cinsiyetim = 'E';
     int yasým = 31;
-
-    bool hayattamý=true;
-
+    int kilo = 72;
+    int boy = 164;
+    float sonuc;
+    int Endeks;
+    int faktoriyel;
     
+    int toplam;
+    int cift;
+    int tek;
+
+    bool hayattamý = true;
+
+    private void Start()
+    {
+        Debug.Log(adým + "  " + soyadým + "  " + adres);
+
+        if (cinsiyetim == 'E')
+        {
+            Debug.Log("Erkek");
+        }
+        else
+        {
+            Debug.Log("Kýz");
+        }
+
+        boykiloendeks();
+        sayi();
+
+    }
+    private void boykiloendeks()
+    {
+        if (Endeks == sonuc)
+        {
+            sonuc = kilo * boy;
+
+        }
+        Debug.Log(Endeks);
 
 
+    }
+    private void sayi()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            toplam = i;
+            Debug.Log("1'den 100'e kadar" + toplam);
+        }
+        for (int i = 0; i < 100;)
+        {
+            cift = i += 2;
+            Debug.Log("çiftler" + cift);
+        }
+        for (int i = -1; i < 99;)
+        {
+            tek = i += 2;
+            Debug.Log("tekler" + tek);
+        }
+       
+
+    }
 }
